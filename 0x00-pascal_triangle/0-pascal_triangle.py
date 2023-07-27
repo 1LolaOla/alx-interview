31fc70aaa86a428777230408ccf1bbca7b13d589
Lola Ola <113696750+1LolaOla@users.noreply.github.com>
2023-07-27 16:06:55 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Pascal's triangle"""
+
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers 
+    """lists of integers 
     representing the Pascal’s triangle of n:
     """
 
