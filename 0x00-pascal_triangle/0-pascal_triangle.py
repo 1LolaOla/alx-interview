113163287b73ffa033eab245724e3a415da598b6
Lola Ola <113696750+1LolaOla@users.noreply.github.com>
2023-07-27 16:26:26 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,28 +2,28 @@
 """Pascal's triangle"""
 
 
-def pascal_triangle(n):
+def pascal_triangle(a):
     """lists of integers 
-    representing the Pascal’s triangle of n:
+    representing the Pascal’s triangle of a:
     """
 
-    if n <= 0:
+    if a <= 0:
         return []
 
     
     """ initialize an empty resulting array """
-    pascal = [[] for idx in range(n)]
+    pascal = [[] for idx in range(a)]
 
-    for li in range(n):
+    for li in range(a):
         for col in range(li+1):
             if(col < li):
                 if(col == 0):
-                    """ the first column is always set to 1 """
+                    """ first column always set to 1 """
                     pascal[li].append(1)
                 else:
                     pascal[li].append(pascal[li-1][col] + pascal[li-1][col-1])
             elif(col == li):
-                """ the diagonal is always set to 1 """
+                """ diagonal always set to 1 """
                 pascal[li].append(1)
 
     return pascal
