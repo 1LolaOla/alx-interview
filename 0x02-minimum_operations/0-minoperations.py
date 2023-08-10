699fc52d6ebb71f55cd6d19b7bc8c21dbda15c71
1LolaOla <funmiolay1@gmail.com>
2023-08-10 09:34:21 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,19 +3,22 @@
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
 
-# Example usage
-n = 9
-print(minOperations(n))  # Output should be 6
+if __name__ == "__main__":
+    n = 4
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
