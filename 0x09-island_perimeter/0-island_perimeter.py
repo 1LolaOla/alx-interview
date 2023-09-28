20deb44a038a2f8ea7f415384f63c3fb65bfc4ad
1LolaOla <funmiolay1@gmail.com>
2023-09-28 23:40:15 +0000

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,48 +1,17 @@
 #!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island described in the grid.
-
-    Args:
-        grid (list[list[int]]): The grid representing the island.
-
-    Returns:
-        int: The perimeter of the island.
+     returns the perimeter of the island described in grid
+    :param grid:
+    :return:
     """
-    perimeter = 0
-
-    if not grid:
-        return perimeter
-
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to perimeter
-
-                # Check adjacent cells (up, down, left, right)
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1  # Subtract for shared edge
-                if i < rows - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 1  # Subtract for shared edge
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1  # Subtract for shared edge
-                if j < cols - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1  # Subtract for shared edge
-
-    return perimeter
-
-# Example usage
-if __name__ == "__main__":
-    grid = [
-        [0, 1, 0, 0],
-        [1, 1, 1, 0],
-        [0, 1, 0, 0],
-        [1, 1, 0, 0]
-    ]
-
-    print(island_perimeter(grid))  # Output: 16
-
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
